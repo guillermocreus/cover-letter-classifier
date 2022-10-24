d5eaa5e59af7e158ca538e73f78cdcf3f120c8e7
Guillermo Creus <guillermocreus98@gmail.com>
2022-10-24 23:13:45 +0200

    delete useless packages

--- setup.py
+++ setup.py
@@ -6,10 +6,7 @@ setup(
     install_requires=[
         'transformers == 4.23.1',
         'transformers-interpret == 0.9.6',
-        'numpy == 1.23.4',
         'torch == 1.12.1',
-        'torchvision == 0.13.1',
-        'torchaudio == 0.12.1',
     ],
     packages=find_packages(
         include=['cover_letter_clf'],
